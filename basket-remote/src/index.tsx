a9afb6829d5e3fb581d056aa9260db9f19e986cf
Rui Miranda <34.ruimiranda@gmail.com>
2025-02-19 09:31:39 +0300

    Update index.tsx

--- basket-remote/src/index.tsx
+++ basket-remote/src/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error("Root element not found. Make sure there's an element with id='root' in your HTML.");
+}
+
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
